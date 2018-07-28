35684a15d80d33f53a4fb6ac132553fadf96bfa3
Deepak Sirone <deepaksirone94@gmail.com>
2018-07-28 19:11:55 +0530

    Add array sum source

---
+++ examples/8051/high_low_volatile.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 1999-2001 Tony Givargis.  Permission to copy is granted
+ * provided that this header remains intact.  This software is provided
+ * with no warranties.
+ *
+ * Version : 2.9
+ */
+
+/*---------------------------------------------------------------------------*/
+
+#include <8052.h>
+
+/*---------------------------------------------------------------------------*/
+unsigned char data[10]; // allocated at 8h
+unsigned char secret[10]; //allocated at 12h
+/*
+void fib(unsigned char n) {
+    
+    unsigned char i;
+    
+    buf[0] = 0;
+    buf[1] = 1;
+    for(i=2; i<n; i++) {
+        buf[i] = buf[i-1] + buf[i-2];
+    }
+}
+*/
+/*---------------------------------------------------------------------------*/
+/*
+void print(unsigned char n) {
+    
+    unsigned char i;
+    
+    for(i=0; i<n; i++) {
+        P0 = buf[i];
+    }
+}
+*/
+/*---------------------------------------------------------------------------*/
+
+void quit() {
+    P0 = P1 = P2 = P3 = 0xDE;
+    P0 = P1 = P2 = P3 = 0xAD;
+    P0 = P1 = P2 = P3 = 0x00;
+    while(1);
+}
+/*---------------------------------------------------------------------------*/
+
+void main() {
+
+	volatile unsigned char i;
+	volatile unsigned char sum = 0;
+	volatile unsigned char n = 10;
+
+	for(i = 0; i < n; i++) {
+		if (!secret[i])
+			sum += data[i];
+	}
+
+	P0 = sum;
+	quit();
+}
